8e6701e6f54cac75ef42c8516407b12be75062ec
Hans Hou <dkplass@gmail.com>
2024-06-05 10:39:45 +0800

    update vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,15 @@ import viteConfig from './vite.config';
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 import { fileURLToPath } from 'node:url';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
-    }
-  })
+export default defineConfig((configEnv) =>
+  mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        environment: 'jsdom',
+        exclude: [...configDefaults.exclude, 'e2e/**'],
+        root: fileURLToPath(new URL('./', import.meta.url))
+      }
+    })
+  )
 );
